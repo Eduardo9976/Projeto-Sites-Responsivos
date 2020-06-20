515e06e23ab4e92f1eac21ebe5996e7bd33a7691
Eduardo9976 <60363926+Eduardo9976@users.noreply.github.com>
2020-06-20 00:40:42 -0300

    ajustando dropMenu

--- js/dropdowm.js
+++ js/dropdowm.js
@@ -1,29 +1,19 @@
 export default function initDropdown(){
   const dataMenu = document.querySelector('[data-dataMenu]')
   const dropDown = document.querySelector('[data-dropMenu]')
+  const html = document.documentElement
 
   const handleClick = (event) => {
     event.preventDefault()
-    dropDown.classList.toggle('ativo')
-    outSideClick(dropDown)
+    if(dataMenu == event.target){
+      dropDown.classList.toggle('ativo')
+    }
+    else {
+      dropDown.classList.remove('ativo')
+    }
   }  
 
-  function outSideClick(dropDown) {
-    const html = document.documentElement
-    if(!dropDown.classList.contains('ativo')) {
-      html.addEventListener('click', handleOutSideClick)
-      dropDown.classList.remove('ativo')
-      dataMenu.removeEventListener('click', handleOutSideClick)
-    }
-    function handleOutSideClick(event) {
-      if(!dropDown.contains(event.target)){
-        dropDown.classList.remove('ativo')
-      } 
-    }
-  }
-
-
-  dataMenu.addEventListener('click', handleClick)
+  html.addEventListener('click', handleClick)
 
 
  
